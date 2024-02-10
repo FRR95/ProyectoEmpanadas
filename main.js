613f9a5c70cc7b608ba8ba54198568781dbd4bda
Fran <franrocher95@gmail.com>
2024-02-10 21:48:49 +0100

    releasing project

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-export const fn = (/* argumentos de la función */) => {
-   // Cuerpo de la función: Completa la lógica de la función aquí.
+export const fn = (a,b,c) => {
+let res=0;
+if(a+b+c<40){
+res=(((a*12)+(b*14)+(c*16))/3).toFixed(0);
+console.log("El resultado es",res);
+}
+else{
+console.log("Lo sentimos,el numero de empanadas no debe superar 40")
+}
 };
+
+
+let arrayEmpanadas=[9,10,11]
+
+fn(...arrayEmpanadas);
