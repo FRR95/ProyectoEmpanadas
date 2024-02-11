f2695a252ee7be470f6374eb8181e07e7262459e
Fran <franrocher95@gmail.com>
2024-02-11 14:18:23 +0100

    refactoring code

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-export const fn = (a,b,c) => {
-let res=0;
-if(a+b+c<40){
-res=(((a*12)+(b*14)+(c*16))/3).toFixed(0);
-console.log("El resultado es",res);
-}
-else{
-console.log("Lo sentimos,el numero de empanadas no debe superar 40")
-}
+export const fn = (a, b, c) => {
+
+    let res = 0;
+    if ((a + b + c) > 39) {
+        console.log("Lo sentimos,el numero de empanadas no debe superar 39")
+    }
+    else if (a < 0 || b < 0 || c < 0) {
+        console.log("Todos los numeros deben de ser positivos")
+    }
+
+    else if ((a + b + c) % 3 != 0) {
+        console.log("Lo sentimos,el numero de empanadas debe de ser divisible entre 3")
+    }
+
+    else {
+        res = Math.ceil(((a * 12) + (b * 14) + (c * 16)) / 3);
+        console.log(res);
+    }
 };
 
 
-let arrayEmpanadas=[9,10,11]
+let arrayEmpanadas = [1, 1, 1];
 
 fn(...arrayEmpanadas);
