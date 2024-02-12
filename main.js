336e8ffe2d05ba5d9472e07abf31d02b0c06e722
FRR95 <franrocher95@gmail.com>
2024-02-12 09:26:31 +0100

    Adding test parameters

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
+import { test } from "vitest";
+
 export const fn = (a, b, c) => {
 
     let res = 0;
     if ((a + b + c) > 39) {
-        console.log("Lo sentimos,el numero de empanadas no debe superar 39")
+        console.log("Lo sentimos,el numero de empanadas no debe superar 39");
+        throw error();
     }
     else if (a < 0 || b < 0 || c < 0) {
-        console.log("Todos los numeros deben de ser positivos")
+        console.log("Todos los numeros deben de ser positivos");
+        throw error();
     }
 
     else if ((a + b + c) % 3 != 0) {
-        console.log("Lo sentimos,el numero de empanadas debe de ser divisible entre 3")
+        console.log("Lo sentimos,el numero de empanadas debe de ser divisible entre 3");
+        throw error();
     }
 
     else {
         res = Math.ceil(((a * 12) + (b * 14) + (c * 16)) / 3);
         console.log(res);
+        return res;
     }
 };
 
